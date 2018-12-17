a186baf8ce4f00e6415f5d5024a8db628da6fb82
Flash2224 <kannamanikiran@gmail.com>
2018-12-17 12:02:39 +0530

    Rays
    Added Rays and from camera to groundplane

---
+++ ShootCapsule/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+
+
+public class Player : MonoBehaviour
+{
+    float moveSpeed = 10;
+
+    Camera viewCamera;
+
+    PlayerController controller;
+
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"),0, Input.GetAxisRaw("Vertical"));
+        Vector3 velocity = input.normalized * moveSpeed;
+        controller.Move(velocity);
+
+        //Creating a Ray that takes input from the camera to mouseposition
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        //creating a groundplane which faces to the vector3 up direction
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        //creating the rayDistance from camera to the plane the ray touches
+        float rayDistance;
+
+        //raycast returns a bool if the ray touches the ground
+        //Casts a ray, from point origin, in direction direction, of length maxDistance, against all colliders in the Scene.
+        //here we are checking the condition if the ray is hitting the ground and once it returns true then cast the ray and see it touches the ground.
+        if (groundPlane.Raycast(ray,out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin, point, Color.blue);
+        }
+    }
+
+    
+    
+}
---
+++ ShootCapsule/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+
+    Vector3 _velocity;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        _velocity = velocity;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + _velocity * Time.fixedDeltaTime);
+    }
+}
